8454cdbf4b1d552c0e12f6bb80f0bcd6999e865d
Christos Mark <baratheoniron@gmail.com>
2025-10-14 19:44:51 -0400

    GET request HELLO WORLD

--- data-backend/main.py
+++ data-backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 @app.get("/")
 def read_root():
-    return {"capturedHello": "capturedWorld"};
+    return {"Hello": "World"};
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
